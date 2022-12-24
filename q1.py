a29ea94eda5c9582adabbea43343f9e9346905d8
douaa.satel <94752163+DouaaS@users.noreply.github.com>
2022-12-24 04:57:36 +0200

    debugged

--- q1.py
+++ q1.py
@@ -9,8 +9,8 @@ import random2 as rd
 def create_strings_to_insert(index):
     empty_lst=[]
     for i in range(index):
-        str =  str(i)
-        empty_lst.append(str)
+        string = str(i)
+        empty_lst.append(string)
     return empty_lst
 
 def run_insertions():
@@ -19,11 +19,13 @@ def run_insertions():
         T = AVLTreeList()
         list_string = create_strings_to_insert(i)
         total = 0
-        for j in range(len(list_string)):
+        curr=0
+        while len(list_string)!=0:
             num = rd.randrange(len(list_string))
-            output = AVLTreeList.insert(j, list_string[num])
+            output = T.insert(curr, list_string[num])
             list_string.remove(list_string[num])
             total += output
+            curr+=1
         res.append(total)
     return res
 def create_AVLTress_for_deletions():
@@ -31,10 +33,12 @@ def create_AVLTress_for_deletions():
     for i in range (1,11):
         T=AVLTreeList()
         list_string = create_strings_to_insert(i)
-        for j in range(len(list_string)):
+        curr=0
+        while len(list_string)!=0:
             num = rd.randrange(len(list_string))
-            AVLTreeList.insert(j, list_string[num])
+            T.insert(curr, list_string[num])
             list_string.remove(list_string[num])
+            curr+=1
         lst.append(T)
     return lst
 
@@ -42,12 +46,11 @@ def run_deletions():
     res=[]
     AVLTrees= create_AVLTress_for_deletions()
     for i in range(1,11):
-        T = AVLTrees[i]
-        options=[i for i in range(T.size())]
+        T = AVLTrees[i-1]
         total=0
-        for j in range(T.size()):
-            num= rd.randrange(T.size())
-            output=AVLTreeList.delete(num)
+        for j in range(T.length()):
+            num = rd.randrange(T.length())
+            output = T.delete(num)
             total+=output
         res.append(total)
     return res
@@ -58,7 +61,7 @@ def insertions_by_n(n):
         string_lst.append(str)
     T = AVLTreeList()
     total = 0
-    for i in range(len(string_lst)):
+    while len(string_lst)!=0:
         num = rd.randrange(len(string_lst))
         output = AVLTreeList.insert(i, string_lst[num])
         string_lst.remove(string_lst[num])
@@ -72,11 +75,11 @@ def deletions_by_n(n):
     for i in range(n):
         str = str(i)
         string_lst.append(str)
-    for i in range(len(string_lst)):
+    while len(string_lst)!=0:
         num = rd.randrange(len(string_lst))
         output = AVLTreeList.insert(i, string_lst[num])
         string_lst.remove(string_lst[num])
-    for i in range(T.size()):
+    for i in range(T.length()):
         num = rd.randrange(num_lst)
         res = T.delete(num)
         num_lst.remove(num)
