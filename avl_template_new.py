d766b6b5a86d3fee16e534c4bc60f6070d0f7908
Nitai Hadad <nitai.hadad@forter.com>
2022-12-28 06:20:01 +0200

    few fixes

--- avl_template_new.py
+++ avl_template_new.py
@@ -320,7 +320,7 @@ class AVLTreeList(object):
 
 		rotations_cnt = 0
 		if left.isRealNode() and right.isRealNode():
-			succ = self.getSucessor(node)
+			succ = self.getSuccessor(node)
 			succ_right_child = succ.getRight()
 			succ_parent = succ.getParent()
 
@@ -655,8 +655,8 @@ class AVLTreeList(object):
 			curr = node
 			while curr.getParent() != None:
 				curr_direction = curr.childDirection()
-				if curr_direction == "LEFT":
-					predecessor = node.getParent()
+				if curr_direction == "RIGHT":
+					predecessor = curr.getParent()
 					break
 				else:
 					curr = curr.getParent()
@@ -667,24 +667,24 @@ class AVLTreeList(object):
 	@rtype: AVLNode
 	@returns: if index(node) = i, return the node in index i-1
 	"""
-	def getSucessor(self,node):
-		sucessor = None
+	def getSuccessor(self, node):
+		successor = None
 		curr = None
 		if node.getRight().isRealNode():
 			curr = node.getRight()
 			while curr.getLeft().isRealNode():
 				curr = curr.getLeft()
-			sucessor = curr
+			successor = curr
 		else:
 			curr = node
 			while curr.getParent() != None:
 				curr_direction = curr.childDirection()
-				if curr_direction == "RIGHT":
-					sucessor = node.getParent()
+				if curr_direction == "LEFT":
+					successor = curr.getParent()
 					break
 				else:
 					curr = curr.getParent()
-		return sucessor
+		return successor
 
 	"""
 	traverse the tree bottom-up, and update the height of each subtree, all the way to the root
@@ -875,6 +875,19 @@ class AVLTreeList(object):
 			i += 1
 		return i
 
+def per_test():
+	t = AVLTreeList()
+	for i in range(100):
+		t.insert(i,i)
+	l1 = t.listToArray()
+	print(l1)
+	l = t.permutation()
+	print(l.listToArray())
+	l.sort()
+	print(l.listToArray())
+
+
+per_test()
 
 
 
